c9dd6d02915e9ba0e171239facf934f3ddd32027
Jony Santos <jonyferreira@hotmail.com>
2017-03-10 13:41:38 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 
 # Online Store API Demo
+
+# Model used
+
+http://www.memoireonline.com/07/12/5995/Online-ordering-and-inventory-system17.png
---
+++ src/main/java/br/com/jonyfs/online/store/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.jonyfs.online.store.repository;
+import br.com.jonyfs.online.store.domain.*;
+/**
+ *
+ * @author jony
+ */
+public interface ProductRepository extends AclRepository<Product, Long> {
+
+}
